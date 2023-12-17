b6c0d6a4d714e116a2c7e3f1410081c10cdfbaa9
Rart3x <ramjattankeny971@gmail.com>
2023-12-17 14:42:04 +0100

    ./Starting/ex00 && ex01 FINISHED

---
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -0,0 +1,35 @@
+import datetime
+
+def check_month(month):
+    switcher = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec"
+    }
+    return switcher.get(month, "error :invalid month")
+
+timesince70 = datetime.datetime.now() - datetime.datetime(1970, 1, 1)
+
+seconds_since_1970 = timesince70.total_seconds()
+
+formatted_seconds_commas = "{:,.4f}".format(seconds_since_1970)
+formatted_seconds_sci = "{:.4e}".format(seconds_since_1970)
+
+print("Seconds since January 1, 1970:", formatted_seconds_commas, "or", formatted_seconds_sci)
+
+##############################################################
+
+today = datetime.date.today()
+month = check_month(today.month)
+data = month + " " + str(today.day) + " " + str(today.year)
+
+print(data)
