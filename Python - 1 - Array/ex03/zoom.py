72e56092c43dee9879d27ef72fed4753e8c56979
Rart3x <ramjattankeny971@gmail.com>
2024-05-26 12:47:39 +0200

    Fix zoom on rotate

--- Python - 1 - Array/ex03/zoom.py
+++ Python - 1 - Array/ex03/zoom.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import cv2
 
 
 def zoom(arr: np.array) -> np.array:
@@ -21,19 +22,19 @@ def display(path: str) -> None:
     '''Display image'''
 
     img = mpimg.imread(path)
-    img = zoom(img)
+    img_resized = zoom(img)
 
-    height, width, mode = img.shape
+    height, width, mode = img_resized.shape
 
-    img2 = img[:, :, 1]
+    gray_img = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
 
-    print(f"\nNew shape after slicing: {width, height, mode} or {img2.shape}")
-    print(img2)
+    print(f"\nNew shape after conversion: {width, height, mode} or {gray_img.shape}")
+    print(gray_img)
 
-    plt.imshow(img2, cmap="gray")
+    plt.imshow(gray_img, cmap="gray")
 
     plt.title("Animal")
-    # plt.show()
+    plt.show()
 
 
 def main():
--- Python - 1 - Array/ex04/rotate.py
+++ Python - 1 - Array/ex04/rotate.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import cv2
 
 
 def zoom(arr: np.array) -> np.array:
@@ -8,9 +9,9 @@ def zoom(arr: np.array) -> np.array:
 
     new_arr = []
 
-    for y in range(500, 100):
+    for y in range(100, 500):
         row = []
-        for x in range(850, 450):
+        for x in range(450, 850):
             row.append(arr[y][x])
         new_arr.append(row)
 
@@ -18,16 +19,19 @@ def zoom(arr: np.array) -> np.array:
 
 
 def display(path: str) -> None:
-    '''Display image zoomed'''
+    '''Display image'''
 
     img = mpimg.imread(path)
-    img = zoom(img)
-    img2 = img[:, :, 1]
+    img_resized = zoom(img)
 
-    print("\nNew shape after slicing: ", img2.shape)
-    print(img2)
+    height, width, mode = img_resized.shape
 
-    plt.imshow(img2, cmap="gray")
+    gray_img = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
+
+    print(f"\nNew shape after conversion: {width, height, mode} or {gray_img.shape}")
+    print(gray_img)
+
+    plt.imshow(gray_img, cmap="gray")
 
     plt.title("Animal")
     plt.show()
