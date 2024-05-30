4a51edc067bf5c60ff4b9299083d73ccabd3d41d
Rart3x <ramjattankeny971@gmail.com>
2024-05-30 13:17:26 +0200

    ex01 done

--- Python - 2 - DataTable/ex01/aff_life.py
+++ Python - 2 - DataTable/ex01/aff_life.py
@@ -6,13 +6,19 @@ def graph(df: []):
     '''Graph function'''
 
     france_data = df[df['country'] == 'France']
+    years = france_data.columns[1:]
+    life_expectancy = france_data.values[0][1:]
 
-    # plt.plot()
-    # plt.title("France Life expectancy Projections")
-    # plt.ylabel("Life expectancy")
-    # plt.xlabel("Year")
-    # plt.grid(True)
-    # plt.show()
+    plt.plot(years, life_expectancy, label='France')
+    plt.title("France Life expectancy Projections")
+
+    plt.xticks(years[::40])
+    plt.yticks(range(30, 101, 10))
+
+    plt.ylabel("Life expectancy")
+    plt.xlabel("Year")
+
+    plt.show()
 
 
 def main():
