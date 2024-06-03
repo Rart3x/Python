cab325dcc3668f8bd219bb73ee54807a4f3f37ae
Rart3x <ramjattankeny971@gmail.com>
2024-06-04 01:23:24 +0200

    ex04 done

--- Python - 3 - OOP/ex04/ft_calculator.py
+++ Python - 3 - OOP/ex04/ft_calculator.py
@@ -1,11 +1,30 @@
 class calculator:
     '''Calculator class'''
 
+    @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
-    #your code here
-    # decorator
+        '''Dot product calculator method'''
+
+        total = 0
+
+        for i in range(len(V1)):
+            total += V1[i] + V2[i]
+        print("Dot product is:", total)
+
     def add_vec(V1: list[float], V2: list[float]) -> None:
-    #your code here
-    # decorator
+        '''Add Vec calculator method'''
+
+        vec = [0] * max(len(V1), len(V2))
+
+        for i in range(len(V1)):
+            vec[i] += V1[i] + V2[i]
+        print("Add Vector is:", vec)
+
     def sous_vec(V1: list[float], V2: list[float]) -> None:
-    #your code here
+        '''Sous Vec calculator method'''
+
+        vec = [0] * max(len(V1), len(V2))
+
+        for i in range(len(V1)):
+            vec[i] += V1[i] - V2[i]
+        print("Sous Vector is:", vec)
