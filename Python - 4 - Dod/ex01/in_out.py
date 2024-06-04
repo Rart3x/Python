4bb5064207496db7a5d829b4f1ba8d778fd80cd4
Rart3x <ramjattankeny971@gmail.com>
2024-06-04 22:24:45 +0200

    ex03 done

--- Python - 4 - Dod/ex01/in_out.py
+++ Python - 4 - Dod/ex01/in_out.py
@@ -13,8 +13,6 @@ def pow(x: int | float) -> int | float:
 def outer(x: int | float, function) -> object:
     '''Outter method'''
 
-    count = 0
-
     def inner() -> float:
         '''Inner method'''
 
--- Python - 4 - Dod/ex02/callLimit.py
+++ Python - 4 - Dod/ex02/callLimit.py
@@ -1,5 +1,6 @@
 def callLimit(limit: int):
     count = 0
+
     def callLimiter(function):
         def limit_function(*args: any, **kwds: any):
             nonlocal count
@@ -11,15 +12,3 @@ def callLimit(limit: int):
                 print("Error:", function, "call to many times")
         return limit_function
     return callLimiter
-
-@callLimit(3)
-def f():
-    print ("f()")
-
-@callLimit(1)
-def g():
-    print ("g()")
-
-for i in range(3):
-    f()
-    g()
